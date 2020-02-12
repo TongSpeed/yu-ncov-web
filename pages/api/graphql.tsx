cb47541cf3930b14a66fdd879b52496f8085624e
fastspeeed@gmail.com <fastspeeed@gmail.com>
2020-02-13 05:57:54 +0800

    add start port

--- pages/api/graphql.tsx
+++ pages/api/graphql.tsx
@@ -1,7 +1,6 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { schema } from '../../server/schema'
 import { createContext } from '../../server/context'
-import {} from '../'
 const apolloServer=new ApolloServer({ schema, context: createContext })
 export const config = {
     api: {
